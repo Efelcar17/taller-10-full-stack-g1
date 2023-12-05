097cb474b622f9a1608371da63099d51d8ccf509
Efelcar17 <caredi777@gmail.com>
2023-12-04 21:55:28 -0500

    punto_1

---
+++ punto-1/index.php
@@ -0,0 +1,16 @@
+<?php
+$name = "Edison";
+const welcome = "Hola";
+echo welcome . " ";
+echo $name . "\n";
+$name = "Felipe";
+echo welcome . " ";
+echo $name . "\n";
+$name = "Edison";
+const welcome_1 = "Hola otra vez";
+echo welcome_1 . " ";
+echo $name . "\n";
+$name = "Daniel";
+echo welcome . " ";
+echo $name;
+?>
